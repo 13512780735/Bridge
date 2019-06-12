27b74a6edbcaa53244e58b3c6b3db9b386de11aa
hl <931317632@qq.com>
2019-06-12 16:59:00 +0800

    20190609

---
+++ app/src/main/java/com/bridgesafe/bridge/ui/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.bridgesafe.bridge.ui.login;
+
+import android.os.Bundle;
+
+import com.bridgesafe.bridge.R;
+import com.bridgesafe.bridge.ui.base.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
